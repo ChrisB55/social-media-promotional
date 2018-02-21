b46903b6c00ed13eef7b89cb7f1ad51a9876dc4a
Christopher W. Brown <christopherwilliambrown@gmail.com>
2018-02-21 14:08:42 -0500

    Add initial files, partial markup
    Vue cli W vuetify configuration

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <input type="radio" name="poll" id="yes" value="Yes">
+          <label for="yes">Yes</label>
+        <input type="radio" name="poll" id="no" value="No">
+          <label for="no">No</label>
+        <br>
+        <input type="submit" value="Vote" class="btn">
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
