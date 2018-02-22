e855cd63a2b4b4f9e674b620ea5bca583dce05f0
Christopher W. Brown <christopherwilliambrown@gmail.com>
2018-02-22 15:19:39 -0500

    Add header, home components
    Remove someboiler plate

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,25 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+<app-header></app-header>
+    <app-home></app-home>
     <v-footer :fixed="fixed" app>
-      <span>&copy; 2017 Publica Now Digitial Services</span>
+      <span>&copy; {{id}}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+
+import Home from './components/Home.vue'
+import Header from './components/Header.vue'
 export default {
+  components: {
+    appHome: Home,
+    appHeader: Header
+  },
   data () {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Let Us Know What You Think'
+      id: '2017 Publica Now Digitial Services'
     }
   },
   name: 'App'
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="container">
+  <v-navigation-drawer app>{{title}}</v-navigation-drawer>
+  <v-toolbar app>{{title}}</v-toolbar>
+  </div>
+</template>
+
+
+<script>
+export default {
+data () {
+    return {
+      title: 'Polling app'
+    }
+  }
+  }
+</script>
+
+
+<style>
+.container {
+    flex:0%;
+    margin: 3em;
+}
+</style>
+
+
